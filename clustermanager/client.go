00de6b3709a52232400e4df7f5d79cb461d5f626
Abhishek Singh <singhabhishek.bit@gmail.com>
2015-08-29 14:43:42 +0000

    client to capture luxmap

---
+++ clustermanager/client.go
@@ -0,0 +1,33 @@
+package client
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
+
+type Map struct {
+	Node struct {
+		ServerList string `json:"serverList"`
+		LuxMap string `json:"luxMap"`
+	} `json:"nodes"`
+}
+
+func Connect(clusterURL string) (string, error) {
+
+	resp, err := http.Get(clusterURL)
+	if err != nil {
+		return "", err
+	}
+
+	//Need to handle this in-case cluster manager dies
+	defer resp.Body.Close()
+	
+	body, err := ioutil.ReadAll(resp.Body)
+
+	var nodes Map	
+	json.Unmarshal([]byte(string(body)), &nodes)
+	vbmap := nodes.Node.LuxMap
+
+	return vbmap, nil
+}
