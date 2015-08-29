a57a341db217a4581ea05b0746d0d56f797a6448
manik <manik.taneja@gmail.com>
2015-08-29 14:38:13 +0530

    Initial commit. Random hacky code.

---
+++ client/client.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/couchbase/gomemcached"
+	"github.com/couchbase/gomemcached/client"
+	"log"
+)
+
+var server = flag.String("server", "localhost", "server URL")
+var port = flag.Int("server port", 11212, "server port")
+
+func main() {
+
+	memServer := fmt.Sprintf("%s:%d", *server, *port)
+	client, err := memcached.Connect("tcp", memServer)
+	if err != nil {
+		log.Printf(" Unable to connect to %v, error %v", memServer, err)
+		return
+	}
+
+	log.Printf(" Connected to server %v", memServer)
+
+	res, err := client.Set(0, "test", 0, 0, []byte("this is a test"))
+	if err != nil || res.Status != gomemcached.SUCCESS {
+		log.Printf("Set failed. Error %v", err)
+		return
+	}
+
+	res, err = client.Get(0, "test")
+	if err != nil || res.Status != gomemcached.SUCCESS {
+		log.Printf("Get failed. Error %v", err)
+		return
+	}
+
+	log.Printf("Get returned %v", res)
+
+}
